a1e70a006af0a7368a388111775a9fc9322d19e8
Octave Victor <victoroctave@gmail.com>
2020-11-24 10:07:53 +0100

    Put credentials in config

--- src/main/java/com/config/JDBCConfiguration.java
+++ src/main/java/com/config/JDBCConfiguration.java
@@ -3,8 +3,11 @@ package com.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class JDBCConfiguration {
+	
+	public static final ResourceBundle CONFIGURATION = ResourceBundle.getBundle("application");
 	
 	private JDBCConfiguration() {
 		
@@ -13,8 +16,8 @@ public class JDBCConfiguration {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		String url = "jdbc:mysql://localhost:3306/twic?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
-		String user = "root";
-		String pass = "root";
+		String user = CONFIGURATION.getString("user");
+		String pass = CONFIGURATION.getString("password");;
 	    return DriverManager.getConnection(url, user, pass);
 	}
 
